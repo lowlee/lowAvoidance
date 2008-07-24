8e6fb09bae0638dbe7195fa324c72211405b027d
Tekkub <tekkub@gmail.com>
2008-07-23 18:25:50 -0600

    Misc cleanup

--- lowAvoidance.lua
+++ lowAvoidance.lua
@@ -13,14 +13,15 @@ local L = {
 	["Hit"] = "Hit",
 }
 
-local result = { };
-result.miss     = 0;
-result.dodge    = 0;
-result.parry    = 0;
-result.block    = 0;
-result.critical = 0;
-result.crushing = 0;
-result.hit      = 0;
+local result = {
+	miss     = 0,
+	dodge    = 0,
+	parry    = 0,
+	block    = 0,
+	critical = 0,
+	crushing = 0,
+	hit      = 0,
+}
 
 local talentedcritreduct = 0;
 local targetlevel = 70;
@@ -30,32 +31,16 @@ local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("lowAvoida
 local f = CreateFrame("Frame")
 
 f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
-f:Hide()
-
-local function GetTalentedCritReduction()
-	local _, class = UnitClass("player");
-	local uncrit = 0;
-
-	-- SoTF
-	if ( class == "DRUID" ) then
-		uncrit = select(5, GetTalentInfo(2, 16));
-	-- SoH
-	elseif ( class == "ROGUE" ) then
-		uncrit = select(5, GetTalentInfo(3, 3));
-	end
-	
-	return uncrit;
-end
 
 local function UpdateHitTable()
 	local level = UnitLevel("player");
 	targetlevel = level + 3;
 	if UnitExists("target") then targetlevel = UnitLevel("target"); end
-	
+
 	local defbase, defbonus = UnitDefense("player");
 	local defskillmod = (defbase + defbonus - targetlevel * 5) * 0.04;
 	local leveldiff = targetlevel - level;
-	
+
 	result.miss = max(5 + defskillmod, 0);
 	result.dodge = GetDodgeChance() - leveldiff * 0.2;
 	result.parry = GetParryChance() - leveldiff * 0.2;
@@ -63,18 +48,18 @@ local function UpdateHitTable()
 	result.critical = max(5 - defskillmod - talentedcritreduct - GetCombatRatingBonus(CR_CRIT_TAKEN_MELEE), 0);
 	result.crushing = max(10 * leveldiff - 15, 0);
 	result.hit = 0;
-	
+
 	local mainhand = GetInventoryItemLink("player", 16);
 	if not mainhand then result.parry = 0; else
-		local mainhandtype = select(9, GetItemInfo(mainhand));
+		local _, _, _, _, _, _, _, _, mainhandtype = GetItemInfo(mainhand);
 		if (mainhandtype ~= "INVTYPE_WEAPON") and (mainhandtype ~= "INVTYPE_WEAPONMAINHAND") and (mainhandtype ~= "INVTYPE_2HWEAPON") then
 			result.parry = 0;
 		end
 	end
-	
+
 	local offhand = GetInventoryItemLink("player", 17);
 	if not offhand then result.block = 0; else
-		local offhandtype = select(9, GetItemInfo(offhand));
+		local _, _, _, _, _, _, _, _, offhandtype = GetItemInfo(offhand);
 		if offhandtype ~= "INVTYPE_SHIELD" then result.block = 0; end
 	end
 
@@ -99,8 +84,8 @@ local function UpdateHitTable()
 	leftover = leftover - result.crushing;
 
 	result.hit = leftover;
-	
-	if result.critical > 0 then 
+
+	if result.critical > 0 then
 		dataobj.text = "|cffff0000"..L["Critable"].."|r";
 	elseif result.crushing > 0 then
 		dataobj.text =  "|cffff8000"..L["Crushable"].."|r";
@@ -110,8 +95,6 @@ local function UpdateHitTable()
 end
 
 function f:PLAYER_LOGIN()
-	self:Show()
-
 	self:RegisterEvent("UNIT_INVENTORY_CHANGED");
 	self:RegisterEvent("UNIT_AURA");
 	self:RegisterEvent("ADDON_LOADED");
@@ -120,24 +103,23 @@ function f:PLAYER_LOGIN()
 	self:UnregisterEvent("PLAYER_LOGIN")
 	self.PLAYER_LOGIN = nil
 
-	self.talentedcritreduct = GetTalentedCritReduction();
+	local _, class = UnitClass("player");
+	local uncrit = 0;
+
+	-- SoTF
+	if ( class == "DRUID" ) then
+		talentedcritreduct = select(5, GetTalentInfo(2, 16));
+	-- SoH
+	elseif ( class == "ROGUE" ) then
+		talentedcritreduct = select(5, GetTalentInfo(3, 3));
+	end
+
 end
 
-function f:UNIT_INVENTORY_CHANGED()
-	UpdateHitTable();
-end
-
-function f:UNIT_AURA()
-	UpdateHitTable();
-end
-
-function f:ADDON_LOADED()
-	UpdateHitTable();
-end
-
-function f:PLAYER_TARGET_CHANGED()
-	UpdateHitTable();
-end
+f.UNIT_INVENTORY_CHANGED = UpdateHitTable
+f.UNIT_AURA = UpdateHitTable
+f.ADDON_LOADED = UpdateHitTable
+f.PLAYER_TARGET_CHANGED = UpdateHitTable
 
 local function GetTipAnchor(frame)
 	local x,y = frame:GetCenter()
@@ -156,17 +138,17 @@ function dataobj.OnEnter(self)
 
 	GameTooltip:AddLine("lowAvoidance")
 	GameTooltip:AddLine(" ")
-	
+
 	GameTooltip:AddLine(L["Defensive Combat Table vs Level"].." "..targetlevel)
 	GameTooltip:AddLine(" ")
-	
-	GameTooltip:AddDoubleLine(L["Miss"], string.format("%.2f", result.miss).."%")
-	GameTooltip:AddDoubleLine(L["Dodge"], string.format("%.2f", result.dodge).."%")
-	GameTooltip:AddDoubleLine(L["Parry"], string.format("%.2f", result.parry).."%")
-	GameTooltip:AddDoubleLine(L["Block"], string.format("%.2f", result.block).."%")
-	GameTooltip:AddDoubleLine(L["Critical"], string.format("%.2f", result.critical).."%")
-	GameTooltip:AddDoubleLine(L["Crushing"], string.format("%.2f", result.crushing).."%")
-	GameTooltip:AddDoubleLine(L["Hit"], string.format("%.2f", result.hit).."%")
+
+	GameTooltip:AddDoubleLine(L["Miss"], string.format("%.2f%%", result.miss))
+	GameTooltip:AddDoubleLine(L["Dodge"], string.format("%.2f%%", result.dodge))
+	GameTooltip:AddDoubleLine(L["Parry"], string.format("%.2f%%", result.parry))
+	GameTooltip:AddDoubleLine(L["Block"], string.format("%.2f%%", result.block))
+	GameTooltip:AddDoubleLine(L["Critical"], string.format("%.2f%%", result.critical))
+	GameTooltip:AddDoubleLine(L["Crushing"], string.format("%.2f%%", result.crushing))
+	GameTooltip:AddDoubleLine(L["Hit"], string.format("%.2f%%", result.hit))
 
 	GameTooltip:Show()
 end
